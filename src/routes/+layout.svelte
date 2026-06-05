b9e074e8ea49d772a73c22259fb7b79f629ac95f
juddisjudd <ipajudd@gmail.com>
2026-06-04 23:43:49 -0700

    feat: remove unused localization code from layout component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,20 +2,8 @@
 	import '@fontsource/inter-tight/400.css';
 	import '@fontsource/inter-tight/500.css';
 	import '@fontsource/inter-tight/600.css';
-	import type { Pathname } from '$app/types';
-	import { resolve } from '$app/paths';
-	import { page } from '$app/state';
-	import { locales, localizeHref } from '$lib/paraglide/runtime';
 
 	let { children } = $props();
 </script>
-
-<div style="display:none">
-	{#each locales as locale (locale)}
-		<a
-			href={resolve(localizeHref(page.url.pathname, { locale }) as Pathname)}
-		>{locale}</a>
-	{/each}
-</div>
 
 {@render children()}
